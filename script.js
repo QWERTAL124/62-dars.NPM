b54d4c4857a4055c56aaca920e690e9c29852dcd
QWERTAL <85424955+QWERTAL124@users.noreply.github.com>
2024-03-28 13:41:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+' use strict '
+
+// Backendda loyihalarni qurish
+
+// Turli xil kutubxonalardan foydalanish jarayonida shu kutubxonalarning o'zlari ham qaysididr kutubxonga qaram bo'ladi
+// Bunday paytda package.json va node modules kabi kutubxonalar yordamga keladi
+
+// bootstrap paketni yuklash
+//npm install bootsstrap
+
+//node_modules papkasini katta loyihalarda ishlatishda githubga joylashda shu papkani joylamaslik kerak
+// Buning uchun .gitigmore fayli ochib, unga node_modulesni yozib qo'yamiz
